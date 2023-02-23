ff9519cf5a63ea22e05b5b9c635db98f3c759f9c
634259025 <103128164+634259025@users.noreply.github.com>
2023-02-23 15:35:32 +0700

    siwakorn

---
+++ student/src/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer  ()  {
+  return (
+    <footer className='footer bg-dark d-flex justify-content-center align-items-center'>
+      <p className='mt-3'> Monly </p>
+    </footer>
+  )
+}
+
+export default Footer
